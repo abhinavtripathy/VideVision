0d880610f90691daf8bcb443c671b42600794511
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2018-12-10 09:13:04 -0500

    Update send_sms.js

--- send_sms.js
+++ send_sms.js
@@ -1,7 +1,7 @@
 const accountSid = 'AC3c4c3ffcbedaa2357041399ba0a42c51';
       const authToken = 'e0c29a99efc06ee5ec82a5df1994bb0d';
       const client = require('twilio')(accountSid, authToken);
-
+      //Twillio SMS
       client.messages
       .create({from: '+19375193594', body: 'body', to: '+19784571217'})
       .then(message => console.log(message.sid))
